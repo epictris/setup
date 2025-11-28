82c89d3d6d39e92c7b2dd619f4d1de0bea3f8c42
Tristan Bray <tpebray@gmail.com>
2025-11-28 23:39:42 +0100

    add ptrace fix to 1password setup

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
-# Installing 1Password & 1Password CLI
+# Install 1Password & 1Password CLI
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 sudo dnf install -y 1password 1password-cli git
+
+# Fix 1Password ptrace scope issue on Fedora (https://support.1password.com/linux-ptrace-scope-issue)
+sudo sed -i '/^kernel.yama.ptrace_scope/d' /etc/sysctl.conf
+sudo sysctl -w kernel.yama.ptrace_scope=1 | sudo tee -a /etc/sysctl.conf
+
+# Authenticate with 1Password
 eval $(op signin)
 op read op://personal/lpvmy5zudeushql4arboiunyhm/'private key' > ~/.ssh/id_ed25519
 op read op://personal/lpvmy5zudeushql4arboiunyhm/'public key' > ~/.ssh/id_ed25519.pub
 chmod 700 ~/.ssh/id_ed25519
 
-# Cloning dotfiles repo
+# Clone dotfiles repo
 git clone --bare git@github.com:epictris/.dotfiles $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull origin main
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --init --recursive --remote --merge
 sudo dnf install -y zsh alacritty neovim fzf chromium crudini
 sudo chsh -s $(which zsh) $USER
 
-# Installing minimal SDDM theme
+# Install minimal SDDM theme
 sudo rm -r /usr/share/sddm/themes/minimal
 sudo cp -r $HOME/.config/sddm-theme/minimal /usr/share/sddm/themes
 sudo cp $HOME/.config/background.jpg /usr/share/backgrounds/background.jpg
